95847eecc02dab339ad82bd8099eee35a35ec034
Eduard Robu <edi_cristi3@yahoo.com>
2021-02-23 12:16:39 +0000

    small update

--- app/Services/UserProgress/CourseStatusInterface.php
+++ app/Services/UserProgress/CourseStatusInterface.php
@@ -12,5 +12,5 @@ interface CourseStatusInterface
 
     public function getStatus();
 
-    public function setLessonsStatus(LessonStatusInterface $lesson_status);
+    public function setChaptersStatus(ChapterStatusInterface $chapter_status);
 }
