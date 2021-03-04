5842e6c61cd1c9fc80c8c2fe859724add32eedc7
Eduard Robu <edi_cristi3@yahoo.com>
2021-03-04 16:05:19 +0000

    Progress Decorator cleanup

--- app/Services/Progress/ChapterProgressInterface.php
+++ app/Services/Progress/ChapterProgressInterface.php
@@ -3,15 +3,7 @@
 namespace App\Services\Progress;
 
 use App\Services\Progress\Decorator\Progress;
-use App\Services\Progress\Wrapper\ProgressWrapperInterface;
 
-interface ChapterProgressInterface
+interface ChapterProgressInterface extends Progress
 {
-    public function setIDs(array $ids): Progress;
-
-    public function setUsersIDs(array $users_id): Progress;
-
-    public function getProgress(): ProgressWrapperInterface;
-
-    public function getChildren() : Progress;
 }
